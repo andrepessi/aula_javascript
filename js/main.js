d3544ae57e4f61cea1ac0ff5e3aeaa60ef371eee
Moacir <andrepessi@gmail.com>
2020-10-23 00:34:22 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var d = new Date();
+console.log(d);
+console.log(d.getUTCMilliseconds());
+
+
+/* var idade = prompt("Informe a sua idade");
+if (idade >= 18){
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}
+*/
+
+
+/* var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"pera", cor:"verde"}];
+console.log(frutas);
+alert("A " + frutas[1].nome + " é " + frutas[0].cor + "?");
+*/
+
+/* var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+
+
+
+
+/* var nome = "André Moacir";
+var sobrenome = "Pessi"
+var cidade = "Porto Alegre";
+var frase = "A seleção do Brasil é a melhor do mundo!";
+//alert("Bem vindo " + nome + " de " + cidade);
+
+console.log(nome);
+console.log(nome + " " + sobrenome);
+console.log(frase.replace("Brasil", "Rio Grande do Sul"));
+console.log(frase.toUpperCase());
+*/
